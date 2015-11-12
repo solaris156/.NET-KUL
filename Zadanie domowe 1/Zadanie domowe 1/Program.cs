241f619798eda1e16ab40a4ba69bb67ba163bacb
solaris156 <zs3.marek.nazar@gmail.com>
2015-11-12 18:45:32 +0100

    ok

---
+++ Zadanie domowe 1/Zadanie domowe 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_domowe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie;
+            Console.WriteLine("Podaj imię");
+            imie = Console.ReadLine();
+            int wiek;
+            Console.WriteLine("Ile masz lat?");
+            wiek = int.Parse(Console.ReadLine());
+            string miasto;
+            Console.WriteLine("Podaj miasto");
+            miasto = Console.ReadLine();
+            Console.WriteLine("Masz na imię: {0}", imie);
+            Console.WriteLine("Masz: {0} lat", wiek);
+            Console.WriteLine("Mieszkasz w {0} ", miasto);
+
+            Console.ReadLine();
+        }
+    }
+}
